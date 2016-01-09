39c5d3d3b3dfd6c0f14bed7c653e09df120d7947
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-08 19:21:26 -0700

    added events

---
+++ events.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The default template for displaying Events
+ *
+ * @package Cacheonium
+ *
+ */
+?>
+
+
+<?php get_header(); ?>
+	<div class="main">
+		<div class="main-body">
+			<div class="row">
+				<div class="columns large-12">
+					<div class="content">
+            <div>
+              <h1>Events Page</h1>
+            </div>
+						<?php get_template_part('content', 'page' ); ?>
+					</div><!-- /.content -->
+
+
+					<!--	<section class="section-comments">
+							<div class="row">
+								<div class="columns small-12">
+
+									<?php// comments_template( '', true ); ?>
+
+								</div>
+							</div>
+						</section>
+					-->
+				</div><!-- /.columns large-8 -->
+
+				<!--
+        <div class="columns large-4">
+						<div class="sidebar">
+							<?php //get_sidebar('page'); ?>
+					 </div> --><!-- /.sidebar
+				</div> /.columns large-4
+        -->
+
+			</div><!-- /.row -->
+		</div><!-- /.main-body -->
+	</div><!-- /.main -->
+<?php get_footer(); ?>
