e10e2501273c2eceeb95e5fc068e7c44a97fde59
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-11 16:13:21 -0700

    get description

--- partials/people.php
+++ partials/people.php
@@ -11,13 +11,23 @@
 
 <article <?php post_class("post post-single"); ?> > <!-- Cache Makers People Block -->
 	<h1>People</h1>
-	
+
 	<div>
 		<h3>Volunteer Leaders</h3>
+
 	</div>
 
 	<div>
 		<h3>Staff</h3>
+		<?php $args = array('role' => 'staff',);
+
+					$users = get_users( $args );
+					foreach($users as $user){
+						echo $user->description;
+					}
+
+
+		 ?>
 	</div>
 
 </article>
