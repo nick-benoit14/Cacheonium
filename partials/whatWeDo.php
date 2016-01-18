a7497489bc8f65ffa85e7a5d30de1761e2d35db7
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-18 10:09:10 -0700

    center divs

--- partials/whatWeDo.php
+++ partials/whatWeDo.php
@@ -1,11 +1,12 @@
 <article <?php post_class("post post-single"); ?> > <!-- Cache Makers People Block -->
 	<div class="cachemakers-content">
      <h2>Things We Do</h2>
-     <div style="display:inline-block; height:200px; width:200px; background-color:blue;"></div>
-     <div style="display:inline-block; height:200px; width:200px; background-color:yellow;"></div>
-     <div style="display:inline-block; height:200px; width:200px; background-color:red;"></div>
-     <div style="display:inline-block; height:200px; width:200px; background-color:green;"></div>
-
+     <div style="text-align:center;">
+       <div style="display:inline-block; height:200px; width:200px; background-color:blue;"></div>
+       <div style="display:inline-block; height:200px; width:200px; background-color:yellow;"></div>
+       <div style="display:inline-block; height:200px; width:200px; background-color:red;"></div>
+       <div style="display:inline-block; height:200px; width:200px; background-color:green;"></div>
+    </div>
   </div>
 
 </article>
