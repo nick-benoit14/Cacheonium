be3411cf7ca9c6cf7a1b5c374fd53e7984d7f389
root <root@WordPress-on-1404.102615-113>
2016-01-18 14:09:34 -0500

    jquery update

--- partials/whatWeDo.php
+++ partials/whatWeDo.php
@@ -2,23 +2,29 @@
 	<div class="cachemakers-content">
      <h2>Things We Do</h2>
      <div style="text-align:center;">
-       <div id="Electronics" class="cachemakers-thingsWeDo"></div>
+       <div id="Electronics" class="cachemakers-thingsWeDo"   ></div>
        <div id="Programming" class="cachemakers-thingsWeDo"></div>
        <div id="Mechanical Design" class="cachemakers-thingsWeDo"></div>
-       <div id="Robotics" class="cachemakers-thingsWeDo active"></div>
+       <div id="Robotics" class="cachemakers-thingsWeDo"></div>
     </div>
 
     <hr style="width:66%; margin:5% auto;">
     <div style="margin:6%; text-align:center;">
       <div class="cachemakers-thingsWeDo"></div>
       <div style="display:inline-block; margin:5%; vertical-align:top; min-width:100px; max-width: 500px;">
-        <h3>The Thing I Clicked</h3>
-        <p>subtext about the thing that i clicked. We have some really cool things going on in relation to the thing you want to hear about.</p>
+        <h3 id="whatWeDo-Header">The Thing I Clicked</h3>
+        <p id="whatWeDo-Description">subtext about the thing that i clicked. We have some really cool things going on in relation to the thing you want to hear about.</p>
       </div>
     </div>
   </div>
-  <script>
-    console.log($('.active').id);
-  </script>
+  <script> 
+  
+   $('.cachemakers-thingsWeDo').click(function(){update(this);});
+   
+   function update(current){
+    $('#whatWeDo-Header').html(current.id);
+    $('#whatWeDo-Description').html(current.dataset.description);
+   }
+ </script>
 
 </article>
