ee8f04ac9f0fb5213ef93ec49ad8bc2f942603cf
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-18 11:19:24 -0700

    sass

--- partials/whatWeDo.php
+++ partials/whatWeDo.php
@@ -10,7 +10,7 @@
 
     <hr style="width:66%; margin:5% auto;">
     <div style="margin:6%; text-align:center;">
-      <div style="background-color:blue; height:200px; width:200px; display:inline-block;"></div>
+      <div class="cachemakers-thingsWeDo"></div>
       <div style="display:inline-block; margin:5%; vertical-align:top; min-width:100px; max-width: 500px;">
         <h3>The Thing I Clicked</h3>
         <p>subtext about the thing that i clicked. We have some really cool things going on in relation to the thing you want to hear about.</p>
