566172af9c1657ca7bf217c6d9df0cb885ee1cbc
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-18 10:59:31 -0700

    more css

--- partials/whatWeDo.php
+++ partials/whatWeDo.php
@@ -7,9 +7,9 @@
        <div style="display:inline-block; height:200px; width:200px; background-color:red;"></div>
        <div style="display:inline-block; height:200px; width:200px; background-color:green;"></div>
     </div>
-    <div style="height:200px; margin:6%;">
-      <div  style="background-color:blue; height:100%; width:200px; display:inline-block;"></div>
-      <div style="display:inline-block; margin:5%; vertical-align:top;width: 500px;">
+    <div style="margin:6%; text-align:center;">
+      <div style="background-color:blue; height:200px; width:200px; display:inline-block;"></div>
+      <div style="display:inline-block; margin:5%; vertical-align:top; width: 500px;">
         <h3>The Thing I Clicked</h3>
         <p>subtext about the thing that i clicked. We have some really cool things going on in relation to the thing you want to hear about.</p>
       </div>
