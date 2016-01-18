616ad462332210a998b921544f9b9a4520134e72
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-18 12:26:38 -0700

    now done

--- partials/whatWeDo.php
+++ partials/whatWeDo.php
@@ -10,7 +10,7 @@
 
     <hr style="width:66%; margin:5% auto;">
     <div style="margin:6%; text-align:center;">
-      <div class="cachemakers-thingsWeDo"></div>
+      <div style="display:inline-block; height:200px; width:200px; background-color:#cccccc; opacity:1;"></div>
       <div style="display:inline-block; margin:5%; vertical-align:top; min-width:100px; max-width: 500px;">
         <h3 id="whatWeDo-Header">The Thing I Clicked</h3>
         <p id="whatWeDo-Description">subtext about the thing that i clicked. We have some really cool things going on in relation to the thing you want to hear about.</p>
