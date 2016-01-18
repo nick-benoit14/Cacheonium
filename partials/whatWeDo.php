884d81e0ca3e2a1f9a9b7336340529bd6b2dfbf4
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-18 10:44:34 -0700

    refloat

--- partials/whatWeDo.php
+++ partials/whatWeDo.php
@@ -1,15 +1,15 @@
 <article <?php post_class("post post-single"); ?> > <!-- Cache Makers People Block -->
 	<div class="cachemakers-content">
      <h2>Things We Do</h2>
-     <div class="row"style="text-align:center;">
+     <div style="text-align:center;">
        <div style="display:inline-block; height:200px; width:200px; background-color:blue;"></div>
        <div style="display:inline-block; height:200px; width:200px; background-color:yellow;"></div>
        <div style="display:inline-block; height:200px; width:200px; background-color:red;"></div>
        <div style="display:inline-block; height:200px; width:200px; background-color:green;"></div>
     </div>
-    <div class="row" style="height:300px;">
-      <div class="col-xs-12 col-md-4" style="background-color: blue; height:100%;"></div>
-      <div class="col-xs-12 col-md-8">
+    <div style="height:300px; margin:6%;">
+      <div  style="background-color:blue; height:100%; width:300px; float:left;"></div>
+      <div>
         <h3>The Thing I Clicked</h3>
         <p>subtext about the thing that i clicked. We have some really cool things going on in relation to the thing you want to hear about.</p>
       </div>
