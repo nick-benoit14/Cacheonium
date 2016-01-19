5c1f2b24177ac96f7fbb69ff5aa53dd1233ec9e8
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-19 12:27:59 -0700

    what we do text

--- partials/whatWeDo.php
+++ partials/whatWeDo.php
@@ -5,22 +5,26 @@
      <p>Cache Makers is all about hands on experimental learning. Here are some of the things we work with on a regular basis.</p>
      <div style="text-align:center;">
        <div id="Electronics" class="cachemakers-thingsWeDo">
-         <p>Electronics. subtext about the thing that i clicked. We have some really cool things going on in relation to the thing you want to hear about.</p>
+         <p>Electronics is a classical pursuit of the maker. No self respecting makerspace would be complete without some cool electrical projects.
+           From basic soldering, blinky christmas tree ornaments, microcontrollers and robotic arms that can draw pictures, there awaits you some cool
+            combination of electrical components that you will think is neat.</p>
          <img src="http://45.55.18.170/wp-content/uploads/2016/01/blueBlock.png">
        </div>
 
        <div id="Programming" class="cachemakers-thingsWeDo">
-         <p>Programming. subtext about the thing that i clicked. We have some really cool things going on in relation to the thing you want to hear about.</p>
+         <p>You may not be allowed to tell your parents, friends, or family what to do. Your computationally inclined electronic friends however are another story.
+            Come learn how to speak with your computer to tell it exactly what you want it to do.</p>
          <img src="http://45.55.18.170/wp-content/uploads/2016/01/g67.png">
        </div>
 
        <div id="Robotics" class="cachemakers-thingsWeDo">
-         <p>Robotics. subtext about the thing that i clicked. We have some really cool things going on in relation to the thing you want to hear about.</p>
+         <p>Researchers have found that robotics might actually be the coolest thing that there is. Come build your own bot that can walk, talk, and do cool stuff.
+            Or maybe start with one that can roll its way along a line you drew on the ground.</p>
          <img src="http://45.55.18.170/wp-content/uploads/2016/01/greenBlock.png">
        </div>
 
        <div id="Mechanical Design" class="cachemakers-thingsWeDo">
-         <p>Mechanical. subtext about the thing that i clicked. We have some really cool things going on in relation to the thing you want to hear about.</p>
+         <p>If you can dream it up, we can make it real. Design the world around you and then turn it into reality with our 3-D printers!</p>
          <img src="http://45.55.18.170/wp-content/uploads/2016/01/yellowBlock.png">
        </div>
 
