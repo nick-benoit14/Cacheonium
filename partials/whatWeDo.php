48472cd8c9538942e2ccccac88a9dbb995ee339f
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-19 11:00:57 -0700

    default about

--- partials/whatWeDo.php
+++ partials/whatWeDo.php
@@ -1,5 +1,5 @@
 <article <?php post_class("post post-single"); ?> > <!-- Cache Makers People Block -->
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> 
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<div class="cachemakers-content">
      <h2>Things We Do</h2>
      <p>Cache Makers is all about hands on experimental learning. Here are some of the things we work with on a regular basis.</p>
@@ -46,6 +46,8 @@
     $('#whatWeDo-Description').html(current.children[0].innerHTML);
     $('#whatWeDo-Image').attr('src', current.children[1].src);
    }
+
+   update(document.getElementById("Electronics"));
  </script>
 
 </article>
